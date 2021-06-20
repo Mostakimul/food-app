617dcb51afdc00233ce05b96a274cd16ed31b195
Mostakimul Karim <58774850+Mostakimul@users.noreply.github.com>
2021-06-21 01:31:13 +0200

    Api Setup for data

--- store/index.js
+++ store/index.js
@@ -142,8 +142,7 @@ export const state = () => ({
         description: "Chinese eggplant filled with ground shrimp in sauce"
       }
     ]
-  }
-]
+  }]
 })
 
 // export const getters = {
@@ -153,13 +152,32 @@ export const state = () => ({
 // }
 
 // export const mutations = {
-//   updateValue: (state, payload) => {
-//     state.value = payload
+//   updateFoodData: (state, data) => {
+//     state.fooddata = data
 //   }
 // }
 
 // export const actions = {
-//   updateActionValue({ commit }) {
-//     commit('updateValue', payload)
+//   async getFoodData({
+//     state, 
+//     commit
+//    }) {
+//      if (state.fooddata.length) return
+//      try {
+//       await fetch('https://dva9vm8f1h.execute-api.us-east-2.amazonaws.com/production/restaurants', {
+//         headers: {
+//           'Content-type': 'application/json',
+//           'x-api-key': process.env.AWS_API_KEY
+//         }
+//       })
+//       .then(response => response.json())
+//       .then(data => {
+//         console.log(data)
+//         // commit('updateFoodData', data)
+//       })
+//      } catch (error) {
+//        console.log(error)
+//      }
+    
 //   }
 // }
