6b53d67badcbba47ec665613cd0270fad3d46b8e
Mostakimul Karim <58774850+Mostakimul@users.noreply.github.com>
2021-07-02 11:45:59 +0200

    Selection for filter added

--- pages/restaurant.vue
+++ pages/restaurant.vue
@@ -2,20 +2,27 @@
   <main class="container restaurant">
     <div class="restaurantheading">
       <h1>Restaurants</h1>
+      <TheSelect @change="selectedRestaurant = $event" />
+      <pre>{{ $data }}</pre>
     </div>
     <TheResturantInfo />
   </main>
 </template>
 
 <script>
-import TheResturantInfo from '@/components/TheResturantInfo.vue'
-
+import TheResturantInfo from '@/components/TheResturantInfo.vue';
+import TheSelect from '@/components/TheSelect.vue';
 export default {
   components: {
+    TheSelect,
     TheResturantInfo,
   },
-}
+  data() {
+    return {
+      selectedRestaurant: '',
+    };
+  },
+};
 </script>
 
-<style lang="scss" scoped>
-</style>
+<style lang="scss" scoped></style>
