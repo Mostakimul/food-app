484f7032f879f50b204727df4be35a3dc76e6cd1
Mostakimul Karim <58774850+Mostakimul@users.noreply.github.com>
2021-07-28 01:18:13 +0200

    vuelidate added for submitting to cart

--- pages/items/_id.vue
+++ pages/items/_id.vue
@@ -25,7 +25,7 @@
             name="option"
             :id="option"
             :value="option"
-            v-model="itemOptions"
+            v-model="$v.itemOptions.$model"
           />
           <label :for="option">{{ option }}</label>
         </div>
@@ -41,16 +41,28 @@
             name="addon"
             :id="addon"
             :value="addon"
-            v-model="itemAddons"
+            v-model="$v.itemAddons.$model"
           />
           <label :for="addon">{{ addon }}</label>
         </div>
       </fieldset>
+      <!-- After submission -->
       <TheToast v-if="cartSubmitted">
         Order submitted!!! <br />
         Checkout more!
         <nuxt-link to="/restaurant">restaurant</nuxt-link></TheToast
       >
+      <!-- If found any error -->
+      <TheToast v-if="errors">
+        Please select options and <br />
+        addons before adding to cart.
+        <nuxt-link to="/restaurant">restaurant</nuxt-link></TheToast
+      >
+
+      <section class="options">
+        <h3>Description</h3>
+        <p>{{ currentItem.description }}</p>
+      </section>
     </section>
   </main>
 </template>
@@ -58,6 +70,7 @@
 <script>
 import { mapState } from 'vuex';
 import TheToast from '@/components/TheToast.vue';
+import { required } from 'vuelidate/lib/validators';
 
 export default {
   components: {
@@ -71,8 +84,19 @@ export default {
       itemAddons: [],
       itemSizeAndCost: [],
       cartSubmitted: false,
+      errors: false,
     };
   },
+  // Validations
+  validations: {
+    itemOptions: {
+      required,
+    },
+    itemAddons: {
+      required,
+    },
+  },
+  // Methods
   methods: {
     addToCart() {
       let formOutput = {
@@ -82,12 +106,23 @@ export default {
         addOns: this.itemAddons,
         combinedPrice: this.combinedPrice,
       };
-      this.cartSubmitted = true;
+      // check if any error
+      let addOnError = this.$v.itemAddons.$invalid;
+      let optionError = this.currentItem.options
+        ? this.$v.itemOptions.$invalid
+        : false;
 
-      // For Store
-      this.$store.commit('addToCart', formOutput);
+      if (addOnError || optionError) {
+        this.errors = true;
+      } else {
+        this.errors = false;
+        this.cartSubmitted = true;
+        // For Store
+        this.$store.commit('addToCart', formOutput);
+      }
     },
   },
+  // Computed
   computed: {
     ...mapState(['fooddata']),
     currentItem() {
